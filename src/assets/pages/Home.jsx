bbf53f441a25ffeeb6b0570bdb4dc5f1d62fa2f9
Zain Ahmed <zainahmed1713@gmail.com>
2023-12-22 20:36:50 +0500

    Fetched notes from Firestore DB

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import CreateNote from "../components/CreateNote";
 import ShowNotes from "../components/ShowNotes";
+import { db } from "../../config/firebase";
+import { getDocs, collection } from "firebase/firestore";
 
 const Home = () => {
+  const notesCollection = collection(db, "notes");
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    const getNotes = async () => {
+      try {
+        const data = await getDocs(notesCollection);
+        const fetchNotes = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+        setNotes(fetchNotes);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getNotes();
+  });
+
   return (
-    <div className="main-container bg-[#E3E8F8] w-screen h-full">
+    <div className="main-container bg-[#E3E8F8] w-screen h-[100vh]">
       <div className="add-note p-2 w-screen">
         <CreateNote />
       </div>
@@ -17,15 +39,12 @@ const Home = () => {
           </h4>
         </div>
         <p className="font-montserrat text-[#203562] font-semibold text-xs ml-3 mt-1 mb-2">
-          Recently viewed
+          Recently added
         </p>
-        {/* {notes &&
-          notes.map((note, index) => { */}
-        {/* return  */}
-        <ShowNotes />;
-        <ShowNotes />;
-        <ShowNotes />;
-        <ShowNotes />;{/* })} */}
+        {notes &&
+          notes.map((note) => {
+            return <ShowNotes {...note} key={note.id} />;
+          })}
       </div>
     </div>
   );
