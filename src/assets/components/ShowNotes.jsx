ad73b2b3eff4624ad81ef2a58e03e7a0c3703048
Zain Ahmed <zainahmed1713@gmail.com>
2023-12-22 20:33:00 +0500

    Notes can now be retrieved from the Firestore DB

--- src/assets/components/ShowNotes.jsx
+++ src/assets/components/ShowNotes.jsx
@@ -1,20 +1,25 @@
-// import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import { db } from "../../config/firebase";
+import { deleteDoc, doc } from "firebase/firestore";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
 
-const ShowNotes = ({ note }) => {
+const ShowNotes = ({ ...notes }) => {
+  const deleteNote = async () => {
+    const notesDoc = doc(db, "notes", notes.id);
+    await deleteDoc(notesDoc);
+  };
   return (
     <div className="notes-container flex flex-col items-center w-screen">
       <div className="note bg-[#F5F5F5] p-3 w-[250px] rounded-xl mb-3">
         <div className="top flex justify-between">
           <h4 className="title font-montserrat font-semibold text-[#203562]">
-            {/* {note.title} */}
-            Test Note #1
+            {notes.title}
           </h4>
           <div className="icons">
             <span className="material-symbols-outlined text-[#203562] hover:opacity-80 cursor-pointer">
               edit
             </span>
             <span
-              // onClick={handleClick}
+              onClick={deleteNote}
               className="material-symbols-outlined text-[#203562] hover:opacity-80 cursor-pointer"
             >
               delete
@@ -24,25 +29,14 @@ const ShowNotes = ({ note }) => {
         <div className="bottom">
           <div className="text-content mb-2">
             <p className="font-montserrat text-xs text-[#303030] tracking-wider">
-              {/* {note.description} */}
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis,
-              excepturi neque? Tenetur ea natus laborum earum eligendi neque,
-              quos aliquid velit porro enim exercitationem, accusantium cumque
-              voluptates? Aperiam, reiciendis repellat! Lorem ipsum dolor, sit
-              amet consectetur adipisicing elit. Veritatis explicabo natus nam
-              sapiente quasi ratione nobis deleniti qui, vel eius reprehenderit?
-              Exercitationem nostrum vero consequatur non id dicta, dignissimos
-              temporibus. Lorem ipsum dolor, sit amet consectetur adipisicing
-              elit. Voluptatibus omnis sit possimus dolores hic id itaque,
-              quisquam alias! Porro facere doloribus nihil atque fuga aspernatur
-              corrupti sapiente quae architecto voluptatum?
+              {notes.description}
             </p>
           </div>
           <div className="time">
             <p className="font-montserrat text-xs opacity-90">
-              {/* {formatDistanceToNow(new Date(note.createdAt), {
+              {formatDistanceToNow(new Date(notes.createdAt), {
                 addSuffix: true,
-              })} */}
+              })}
             </p>
           </div>
         </div>
